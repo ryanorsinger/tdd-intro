d22986f87591ce6f36cb310d2ec59005927b960d
RyanOrsinger <ryan.orsinger@gmail.com>
2018-09-26 16:31:49 -0500

    move new Stack() instantiation to @Before

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -5,16 +5,20 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class StackTest {
+    Stack stack;
+
+    @Before
+    public void initialize() {
+        stack = new Stack();
+    }
 
     @Test
     public void testIsEmpty() {
-        Stack stack = new Stack();
         assertTrue(stack.isEmpty());
     }
 
     @Test
     public void testPush() {
-        Stack stack = new Stack();
         assertTrue(stack.isEmpty());
         stack.push("Cat");
         assertFalse(stack.isEmpty());
